545555e800596202724e5f8a0aaa146a3830f93c
Aparna Unni <aparnaunni28@gmail.com>
2026-01-05 15:41:53 +0530

    Initial Commit My Portfolio

---
+++ src/components/sections/Contact.jsx
@@ -0,0 +1,118 @@
+import { useState } from "react";
+import { RevealOnScroll } from "../RevealOnScroll";
+import emailjs from "emailjs-com";
+import {
+  Instagram,
+  Linkedin,
+  Mail,
+  MapPin,
+  Phone,
+  Send,
+  Twitch,
+  Twitter,
+  Github,
+} from "lucide-react";
+
+export const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMPLATE_ID,
+        e.target,
+        import.meta.env.VITE_PUBLIC_KEY
+      )
+      .then((result) => {
+        alert("Message Sent!");
+        setFormData({ name: "", email: "", message: "" });
+      })
+      .catch(() => alert("Oops! Something went wrong. Please try again."));
+  };
+
+  return (
+    <section
+      id="contact"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="px-4 w-full min-w-[300px] md:w-[500px] sm:w-2/3 p-6">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
+            Get In Touch
+          </h2>
+            <div className="space-y-8 glass p-6 rounded-xl border border-white/10 ">
+
+            <div className="space-y-6 justify-center">
+              <div className="flex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Mail className="h-6 w-6 text-primary" />{" "}
+                </div>
+                <div>
+                  <h4 className="font-medium"> Email</h4>
+                  <a
+                    href="mailto:hello@gmail.com"
+                    className="text-muted-foreground hover:text-primary transition-colors"
+                  >
+                    aparnaunni28@gmail.com
+                  </a>
+                </div>
+              </div>
+              <div className="flex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Phone className="h-6 w-6 text-primary" />{" "}
+                </div>
+                <div>
+                  <h4 className="font-medium"> Phone</h4>
+                  <a
+                    href="tel:+11234567890"
+                    className="text-muted-foreground hover:text-primary transition-colors"
+                  >
+                    +971 54 393 0747
+                  </a>
+                </div>
+              </div>
+              <div className="flex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <MapPin className="h-6 w-6 text-primary" />{" "}
+                </div>
+                <div>
+                  <h4 className="font-medium"> Location</h4>
+                  <a className="text-muted-foreground hover:text-primary transition-colors">
+                    Silicon Oasis,Dubai,UAE
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="pt-8">
+              <h4 className="font-medium mb-4"> Connect With Me</h4>
+              <div className="flex space-x-4 justify-center">
+                <a href="https://www.linkedin.com/in/aparna-unni-928514225" target="_blank">
+                  <Linkedin />
+                </a>
+                <a href="https://github.com/AparnaUnni" target="_blank">
+                  <Github/>
+                </a>
+                
+                
+                
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
+
+
+
